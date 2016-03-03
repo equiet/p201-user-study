1abaa6024df8ccc4980ceabcc0a5fc9ff3538582
Jakub Jurových <jakub.jurovych@gmail.com>
2016-03-03 11:19:13 +0000

    Add Task 3

---
+++ Task 3/js/model.js
@@ -0,0 +1,42 @@
+(function (window) {
+	'use strict';
+
+
+	/**
+	 * Creates a new Model instance and hooks up the storage.
+	 *
+	 * @constructor
+	 */
+	function Model() {
+		this.storage = [];
+	}
+
+	/**
+	 * Creates a new todo model
+	 *
+	 * @param {string} [title] The title of the task
+	 * @param {function} [callback] The callback to fire after the model is created
+	 */
+	Model.prototype.create = function (title, callback) {
+		title = title || '';
+		callback = callback || function () {};
+
+		var newItem = {
+			title: title.trim(),
+			completed: false
+		};
+
+		this.storage.push(newItem);
+		callback.call(this, [newItem])
+	};
+
+	/**
+	 * Finds and returns a model in storage.
+	 */
+	Model.prototype.read = function () {
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.Model = Model;
+})(window);
