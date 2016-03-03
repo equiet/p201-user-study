8f55ac38e2692ccb96de96bb0be8647dd5e737a5
Jakub Jurových <jakub.jurovych@gmail.com>
2016-03-03 10:57:59 +0000

    Simplify code

--- js/app.js
+++ js/app.js
@@ -8,8 +8,7 @@
 	 * @param {string} name The name of your new to do list.
 	 */
 	function Todo(name) {
-		this.storage = new app.Store(name);
-		this.model = new app.Model(this.storage);
+		this.model = new app.Model();
 		this.template = new app.Template();
 		this.view = new app.View(this.template);
 		this.controller = new app.Controller(this.model, this.view);
@@ -17,9 +16,5 @@
 
 	var todo = new Todo('todos-vanillajs');
 
-	function setView() {
-		todo.controller.setView(document.location.hash);
-	}
-	$on(window, 'load', setView);
-	$on(window, 'hashchange', setView);
+	$on(window, 'load', function() { todo.controller.showAll(); });
 })();
--- js/template.js
+++ js/template.js
@@ -31,20 +31,15 @@
 	 */
 	function Template() {
 		this.defaultTemplate
-		=	'<li data-id="{{id}}" class="{{completed}}">'
+		=	'<li>'
 		+		'<div class="view">'
-		+			'<input class="toggle" type="checkbox" {{checked}}>'
 		+			'<label>{{title}}</label>'
-		+			'<button class="destroy"></button>'
 		+		'</div>'
 		+	'</li>';
 	}
 
 	/**
 	 * Creates an <li> HTML string and returns it for placement in your app.
-	 *
-	 * NOTE: In real life you should be using a templating engine such as Mustache
-	 * or Handlebars, however, this is a vanilla JS example.
 	 *
 	 * @param {object} data The object containing keys you want to find in the
 	 *                      template to replace.
@@ -63,49 +58,11 @@
 
 		for (i = 0, l = data.length; i < l; i++) {
 			var template = this.defaultTemplate;
-			var completed = '';
-			var checked = '';
-
-			if (data[i].completed) {
-				completed = 'completed';
-				checked = 'checked';
-			}
-
-			template = template.replace('{{id}}', data[i].id);
 			template = template.replace('{{title}}', escape(data[i].title));
-			template = template.replace('{{completed}}', completed);
-			template = template.replace('{{checked}}', checked);
-
 			view = view + template;
 		}
 
 		return view;
-	};
-
-	/**
-	 * Displays a counter of how many to dos are left to complete
-	 *
-	 * @param {number} activeTodos The number of active todos.
-	 * @returns {string} String containing the count
-	 */
-	Template.prototype.itemCounter = function (activeTodos) {
-		var plural = activeTodos === 1 ? '' : 's';
-
-		return '<strong>' + activeTodos + '</strong> item' + plural + ' left';
-	};
-
-	/**
-	 * Updates the text within the "Clear completed" button
-	 *
-	 * @param  {[type]} completedTodos The number of completed todos.
-	 * @returns {string} String containing the count
-	 */
-	Template.prototype.clearCompletedButton = function (completedTodos) {
-		if (completedTodos > 0) {
-			return 'Clear completed';
-		} else {
-			return '';
-		}
 	};
 
 	// Export to window
